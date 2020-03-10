3f5d09a90e4ea6d518cb11dd551f210a9a6ff48d
James Thimont <james.thimont@machinemax.com>
2020-03-10 19:23:06 +0000

    Remove K8s provider

--- modules/beta-private-cluster-update-variant/auth.tf
+++ modules/beta-private-cluster-update-variant/auth.tf
@@ -19,17 +19,18 @@
 /******************************************
   Retrieve authentication token
  *****************************************/
-data "google_client_config" "default" {
-  provider = google-beta
-}
+# data "google_client_config" "default" {
+#   provider = google-beta
+# }
 
 /******************************************
   Configure provider
  *****************************************/
-provider "kubernetes" {
-  version                = "~> 1.10.0"
-  load_config_file       = false
-  host                   = "https://${local.cluster_endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
-}
+# provider "kubernetes" {
+#   version                = "~> 1.10.0"
+#   load_config_file       = false
+#   host                   = "https://${local.cluster_endpoint}"
+#   token                  = data.google_client_config.default.access_token
+#   cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
+# }
+
