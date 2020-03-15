1f4483bc1a66ba33cd1ced0d45dc73328be7c104
James Thimont <james.thimont@machinemax.com>
2020-03-15 20:09:04 +0000

    Remove auto_upgrade variable

--- modules/safer-cluster-update-variant/main.tf
+++ modules/safer-cluster-update-variant/main.tf
@@ -39,7 +39,6 @@ module "gke" {
   //
   // https://cloud.google.com/kubernetes-engine/versioning-and-upgrades
   node_version = ""
-  auto_upgrade = true
 
   master_authorized_networks = var.master_authorized_networks
 
